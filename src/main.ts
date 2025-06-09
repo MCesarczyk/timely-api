49dfa1751928a0a0aa2ec1bf324ddab873d3b872
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2025-06-10 01:55:45 +0200

    feat: configure app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+
+  app.enableCors(['http://localhost:4200', 'http://localhost:3100']);
+
+  const port = process.env.PORT || 3333;
+  await app.listen(port, '0.0.0.0');
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
 }
-bootstrap();
+
+void bootstrap();
